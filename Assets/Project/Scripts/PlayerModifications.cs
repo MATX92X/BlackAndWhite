c5cd4e68a8c18882bd0f9374876ea2c1292ef845
Mirco5290 <mirco.orsolin@gmail.com>
2016-04-23 17:43:52 +0200

    Reazioni al cibo
    Aggiunto commenti, creato BadFood, leggero cambio di posizione della
    camera

--- Assets/Project/Scripts/PlayerModifications.cs
+++ Assets/Project/Scripts/PlayerModifications.cs
@@ -3,21 +3,33 @@ using System.Collections;
 
 public class PlayerModifications : MonoBehaviour
 {
-    public string Tag;
+    public string GoodTag;
+    public string BadTag;
     public float ContinueDecrease;
     public float GoodIncrease;
+    public float BadIncrease;
 
+    //GoodIncrease
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == Tag)
+        //Verifico che il trigger sia di un oggetto con Tag == GoodFood
+        if (other.gameObject.tag == GoodTag)
         {
+            //Aumento di dimensioni solo in x&y z resta costante
             transform.localScale += new Vector3(GoodIncrease, GoodIncrease, 0);
+            Destroy(other.gameObject);
+        }
+        //Verifico che il trigger sia di un oggetto con Tag == BadFood
+        if (other.gameObject.tag == BadTag)
+        {
+            //Aumento di dimensioni solo in x&y z resta costante
+            transform.localScale += new Vector3(BadIncrease, BadIncrease, 0);
             Destroy(other.gameObject);
         }
     }
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update ()
     {
         if ((transform.localScale.x) > 0.5)
         {
